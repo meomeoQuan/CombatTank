dfc776a3ac2bd27b179a165e08b09ed1d2a0accb
HuyTP-356 <huytpde180356@fpt.edu.vn>
2025-10-02 22:28:20 +0700

    fix factory

--- Assets/Scripts/Models/Factory/CharacterFactory.cs
+++ Assets/Scripts/Models/Factory/CharacterFactory.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using Assets.Scripts.Models.Characters;
+using Assets.Scripts.Models.Equipments;
+
+namespace Assets.Scripts.Models.Factory
+{
+    public static class CharacterFactory
+    {
+        public static Character CreateCharacterA()
+        {
+            var character = new Character
+            {
+                Name = "A",
+                BaseHP = 100,
+                BaseATK = 5,
+                BaseSpeed = 2,
+                BaseDodge = 0,
+                BaseArmor = 1,
+                BaseRegen = 0
+            };
+
+
+            return character;
+        }
+
+        public static Character CreateCharacterB()
+        {
+            var character = new Character
+            {
+                Name = "B",
+                BaseHP = 120,
+                BaseATK = 4,
+                BaseSpeed = 2,
+                BaseDodge = 0,
+                BaseArmor = 1,
+                BaseRegen = 0
+            };
+
+
+
+            return character;
+        }
+    }
+}

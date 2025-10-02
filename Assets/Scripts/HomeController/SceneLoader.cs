64c1d786ca95620151a63234b16ed0cca9775a58
Martin-Huỳnh <85268766+MartinDT-TH@users.noreply.github.com>
2025-10-02 22:01:57 +0700

    Update SceneLoader

--- Assets/Scripts/HomeController/SceneLoader.cs
+++ Assets/Scripts/HomeController/SceneLoader.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Hàm này sẽ được gọi khi bạn nhấn nút
+    public void LoadNewScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}

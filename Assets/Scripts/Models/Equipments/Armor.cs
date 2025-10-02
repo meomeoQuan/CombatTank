56d84f8e9dbb1b6425633abe032b7584ed5bc3ee
HuyTP-356 <huytpde180356@fpt.edu.vn>
2025-10-02 22:19:16 +0700

    fix armor

--- Assets/Scripts/Models/Equipments/Armor.cs
+++ Assets/Scripts/Models/Equipments/Armor.cs
@@ -1,1 +1,18 @@
 ﻿
+using UnityEngine;
+
+namespace Assets.Scripts.Models.Equipments
+{
+    //  HULL ARMOR: THÂN GIÁP CHÍNH
+    public class Armor : EquipmentBase
+    {
+        public Armor(string id, string name, Sprite icon, float hpPercent = 0, float armorPercent = 0, float regenFlat = 0)
+            : base(id, name, icon)
+        {
+            if (hpPercent != 0) AddPercentBonus(StatType.HP, hpPercent);
+            if (armorPercent != 0) AddPercentBonus(StatType.Armor, armorPercent);
+            if (regenFlat != 0) AddFlatBonus(StatType.Regen, regenFlat);
+        }
+    }
+}
+

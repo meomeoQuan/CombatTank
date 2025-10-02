50c74adcd18de74f241e20b628e630430cb8fdbf
HuyTP-356 <huytpde180356@fpt.edu.vn>
2025-10-02 22:24:15 +0700

    fix pant

--- Assets/Scripts/Models/Equipments/Pants.cs
+++ Assets/Scripts/Models/Equipments/Pants.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Models.Equipments
+{
+    //ENGINE: HỆ THỐNG ĐỘNG CƠ 
+    public class Pants : EquipmentBase
+    {
+        public Pants(
+            string id,
+            string name,
+            Sprite icon,
+            float hpPercent = 0,
+            float armorPercent = 0
+        ) : base(id, name, icon)
+        {
+            if (hpPercent != 0) AddPercentBonus(StatType.HP, hpPercent);
+            if (armorPercent != 0) AddPercentBonus(StatType.Armor, armorPercent);
+        }
+    }
+}

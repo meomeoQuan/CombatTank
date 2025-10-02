abebb86f1119af072f830ce15663bb872e9c5364
HuyTP-356 <huytpde180356@fpt.edu.vn>
2025-10-02 22:25:52 +0700

    fix shoes

--- Assets/Scripts/Models/Equipments/Shoes.cs
+++ Assets/Scripts/Models/Equipments/Shoes.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Models.Equipments
+{
+    public class Shoes : EquipmentBase
+    {
+        // TRACKS: BÁNH XÍCH
+        public Shoes(
+            string id,
+            string name,
+            Sprite icon,
+            float speedPercent = 0,
+            float dodgePercent = 0,
+            float regenFlat = 0
+        ) : base(id, name, icon)
+        {
+            if (speedPercent != 0) AddPercentBonus(StatType.Speed, speedPercent);
+            if (dodgePercent != 0) AddPercentBonus(StatType.Dodge, dodgePercent);
+            if (regenFlat != 0) AddFlatBonus(StatType.Regen, regenFlat);
+        }
+    }
+}

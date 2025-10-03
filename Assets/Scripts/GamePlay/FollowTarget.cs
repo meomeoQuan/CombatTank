d7dd89b1558314ec5de247774085424828e6f757
Anh <anhbanmai2004@gmail.com>
2025-10-03 13:04:18 +0700

    Fix FolowTarget

--- Assets/Scripts/GamePlay/FollowTarget.cs
+++ Assets/Scripts/GamePlay/FollowTarget.cs
@@ -1,16 +1,1 @@
-﻿using UnityEngine;
-
-public class FollowTarget : MonoBehaviour
-{
-    [Tooltip("Đối tượng cần follow (ví dụ Object A)")]
-    public Transform target; 
-    public Vector3 offset = new Vector3(0, 0, 0);
-    void Update()
-    {
-        if (target != null)
-        {
-            // Luôn lấy vị trí target cộng với offset
-            transform.position = target.position + offset;
-        }
-    }
-}
+﻿

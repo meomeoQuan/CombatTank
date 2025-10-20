aa272d66c9fc82fd904b7234542b01dbe0023e57
Anh <anhbanmai2004@gmail.com>
2025-10-20 13:58:12 +0700

    Add script enemy movement, player awareness controller / CHANGE in GamePlay in Folder Scipts

---
+++ Assets/Scripts/GamePlay/EnemyMovement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private float _speed; //tốc độ của kẻ thù
+
+    [SerializeField]
+    private float _rotationSpeed;
+    private Rigidbody2D _rigibody; //muốn di chuyển kẻ thù
+    private PlayerAwarenessController _playerAwarenessController;
+    private Vector2 _targetDriection; //hướng mục tiêu
+
+    private void Awake()
+    {
+        _rigibody = GetComponent<Rigidbody2D>();
+        _playerAwarenessController = GetComponent<PlayerAwarenessController>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        UpdateTargetDirection(); 
+        RotateTowardsTarget();
+        SetVelocity();
+    }
+
+    private void UpdateTargetDirection() //cập nhật hướng mục tiêu 
+    {
+        if (_playerAwarenessController.AwareOfPlayer) //ktra xem enemy có nhận được player không
+        {
+            _targetDriection = _playerAwarenessController.DirectionToPlayer; //nếu có thì hướng mục tiêu theo hướng của player
+        }
+        else
+        {
+            _targetDriection = Vector2.zero; //nếu không thì hướng mục tiêu là 0
+        }
+    }
+
+    private void RotateTowardsTarget() //xoay hướng về phía mục tiêu
+    {  
+        //kiểm tra trước xem hướng mục tiêu có bằng 0 không 
+        if(_targetDriection == Vector2.zero)
+        {
+            return; //nếu bằng 0 thì không làm gì cả
+        }
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDriection); //tạo một quaternion để xoay về hướng mục tiêu
+        Quaternion newRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime); //tạo một quaternion mới để xoay từ vị trí hiện tại đến vị trí mục tiêu với tốc độ xoay
+        _rigibody.SetRotation(newRotation); //đặt vị trí xoay mới cho rigidbody
+    }
+
+    private void SetVelocity() //đặt vận tốc
+    {
+        if(_targetDriection == Vector2.zero)
+        {
+            _rigibody.linearVelocity = Vector2.zero; 
+        }
+        else
+        {
+            _rigibody.linearVelocity = transform.up * _speed;
+        }
+    }
+}
---
+++ Assets/Scripts/GamePlay/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float _rotationSpeed;
+    private Rigidbody2D _rigibody;
+    private Vector2 _movementInput; //biến nhận input di chuyển
+    private Vector2 _smoothedMovementInput;
+    private Vector2 _smoothedMovementVelocity;
+    private void Awake()
+    {
+        _rigibody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        SetPlayerVelocity();
+        RotateInDirectionOfInput();
+    }
+
+    private void SetPlayerVelocity()
+    {
+        _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput, ref _smoothedMovementVelocity, 0.1f);
+        _rigibody.linearVelocity = _smoothedMovementInput * _speed; //gán vận tốc cho rigidbody bằng với input di chuyển
+    }
+
+    private void RotateInDirectionOfInput() //kiểm tra đầu vào có bất kì chuyển động nào không
+    {
+        if(_movementInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput); //tạo một quaternion để xoay về hướng mục tiêu
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime); //biến cho phép xoay 
+            _rigibody.MoveRotation(rotation); //áp dụng vị trí mới cho vật thể rắn
+        }
+    }
+
+    private void OnMove(InputValue inputValue) //nhận tham số từ input system
+    {
+        _movementInput = inputValue.Get<Vector2>(); 
+    }
+}

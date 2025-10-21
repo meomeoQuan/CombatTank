f12a3993ccc4689f3b982cee47a7bf5f87892bc2
Martin-Huỳnh <85268766+MartinDT-TH@users.noreply.github.com>
2025-10-21 21:00:32 +0700

    ok

--- Assets/Scenes/MainMenuUI.cs
+++ Assets/Scenes/MainMenuUI.cs
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
@@ -16,6 +17,24 @@ public class NewMonoBehaviourScript : MonoBehaviour
         
     }
 }
+=======
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+>>>>>>> parent of 520a9b8 (Merge remote-tracking branch 'origin/thanh' into Tho)
 =======
 using UnityEngine;
 

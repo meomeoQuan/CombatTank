b1072f6f0926f0e406f94a933593b9c44a536dae
Anh <anhbanmai2004@gmail.com>
2025-11-11 15:48:53 +0700

    Add thêm vài lớp phụ cho enemy spawner, lấy lại logic của enemy alien Thêm layer: enemy Modifed prefab của collectable, health collectable

---
+++ Assets/Scripts/GamePlay/SpawnerCollectableDrop.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class SpawnerCollectableDrop : MonoBehaviour
+{
+    [SerializeField] private float _chanceOfCollectableDrop;
+    private CollectableSpawner _collectableSpawner;
+    private SpawnerHealthController _healthController;
+
+    private void Awake()
+    {
+        _collectableSpawner = FindFirstObjectByType<CollectableSpawner>();
+    }
+
+    private void Start()
+    {
+        _healthController = GetComponent<SpawnerHealthController>();
+        if (_healthController != null)
+        {
+            _healthController.OnDied.AddListener(OnSpawnerDied);
+        }
+    }
+
+    private void OnSpawnerDied()
+    {
+        float random = Random.Range(0f, 1f);
+        if (_chanceOfCollectableDrop >= random && _collectableSpawner != null)
+        {
+            _collectableSpawner.SpawnCollectable(transform.position);
+        }
+    }
+    public void RandomlyDropCollectable()
+    {
+        Debug.Log($"[Drop] OnDied được gọi tại {name}");
+        float random = Random.Range(0f, 1f);
+        Debug.Log($"[Drop] Random={random}, Chance={_chanceOfCollectableDrop}");
+
+        if (_chanceOfCollectableDrop >= random)
+        {
+            if (_collectableSpawner != null)
+            {
+                Debug.Log("[Drop] Spawning collectable at " + transform.position);
+                _collectableSpawner.SpawnCollectable(transform.position);
+            }
+            else
+            {
+                Debug.LogWarning("[Drop] Không tìm thấy CollectableSpawner trong scene!");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GamePlay/SpawnerHealthController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class SpawnerHealthController : MonoBehaviour
+{
+    [SerializeField]
+    private float _currentHealth;
+
+    [SerializeField]
+    private float _maximumHealth;
+
+    public float RemainingHealthPercentage
+    {
+        get
+        {
+            return _currentHealth / _maximumHealth;
+        }
+    }
+
+    public bool IsInvincible { get; set; } // cho phép bất khả chiến bại sau khi nhận sát thương
+
+    public UnityEvent OnDied;    // sự kiện khi spawner chết
+    public UnityEvent OnDamaged; // sự kiện khi spawner nhận sát thương
+
+    public void TakeDamage(float damageAmount)
+    {
+        Debug.Log($"[SpawnerHealth] {name} nhận {damageAmount} damage");
+
+        if (_currentHealth == 0)
+            return;
+
+        if (IsInvincible)
+            return;
+
+        _currentHealth -= damageAmount;
+        Debug.Log($"[SpawnerHealth] Máu còn lại: {_currentHealth}");
+
+        if (_currentHealth <= 0)
+        {
+            _currentHealth = 0;
+            Debug.Log("[SpawnerHealth] Gọi OnDied");
+            OnDied.Invoke();
+        }
+        else
+        {
+            OnDamaged.Invoke();
+        }
+    }
+
+    public void AddHealth(float amountToAdd)
+    {
+        if (_currentHealth == _maximumHealth)
+            return;
+
+        _currentHealth += amountToAdd;
+
+        if (_currentHealth > _maximumHealth)
+        {
+            _currentHealth = _maximumHealth;
+        }
+    }
+}
